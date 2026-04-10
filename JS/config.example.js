16e1639a2040a1ae3c63d4f412a1d5b11fa18f39
Lucas Loutayf <loutayflucas@gmail.com>
2026-04-10 16:08:28 -0300

    fix imagenes

---
+++ JS/config.example.js
@@ -0,0 +1,5 @@
+// Copiá este archivo como config.js y completá con tu propia API key.
+// NUNCA subas config.js al repositorio (está en .gitignore).
+const CONFIG = {
+  GOOGLE_MAPS_API_KEY: "TU_API_KEY_AQUI"
+};
